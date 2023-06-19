e79322b06fa4b0c8c0b7bdf68e03a2b1338638b9
Erika Contreras <ericontrerasleal2@gmail.com>
2023-06-19 16:29:02 -0500

    Adds the functionality to remove a movie from the watch list and show it in seen list

--- src/App.js
+++ src/App.js
@@ -4,33 +4,43 @@ import './App.css';
 
 const App = () => {
   const [movies, setMovies] = useState(movieList);
-  
+  const [seenMovies, setSeenMovies] = useState([]);
+
+  const deleteMovie = movieId => {
+    setSeenMovies(mv => [...mv, movies.find(mv => mv.id === movieId)]);
+    setMovies(movies.filter(mv => mv.id !== movieId));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie of the week</h1>
-        
       </header>
-      <div>
+      <body>
       <h2><b>To see</b></h2>
-      <p>
+      <ul>
       {
           movies.map(movie => (
-            <div>
-              <p>{movie.name}, {movie.year}</p>
-              <p></p>
+            <div key={movie.id}>
+              <p>{movie.name}</p>
+              <p>{movie.year}</p>
+              <button onClick = {() => deleteMovie(movie.id)}>Mark as seen</button>
             </div>
-            
           ))
-        }
-      </p>
+      }
+      </ul>
       <h2><b>Seen</b></h2>
       <p>
       {
-          
+        seenMovies.map(movie => (
+          <div key={movie.id}>
+              <p>{movie.name}</p>
+              <p>{movie.year}</p>
+        </div>
+        ))
         }
       </p>
-      </div>
+      </body>
     </div>
   );
 }
