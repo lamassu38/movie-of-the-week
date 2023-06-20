5fc1cde98f135ee4579e6d0976f1dad0d7d0d137
Erika Contreras <ericontrerasleal2@gmail.com>
2023-06-20 13:25:27 -0500

    Add functionality to choose a random movie. Add the selectedMovie states and the drawRandomItem, getRandomIndex and handleRandomOptionClick functions to the App component and update de JXS code to show an random option

--- src/App.js
+++ src/App.js
@@ -5,16 +5,42 @@ import './App.css';
 const App = () => {
   const [movies, setMovies] = useState(movieList);
   const [seenMovies, setSeenMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState('');
 
   const deleteMovie = movieId => {
     setSeenMovies(mv => [...mv, movies.find(mv => mv.id === movieId)]);
     setMovies(movies.filter(mv => mv.id !== movieId));
   };
 
+  const handleSelectedMovieClick = () => {
+    const selectMovie = drawRandomItem(movieList);
+    setSelectedMovie(selectMovie);
+  };
+
+  const drawRandomItem = arr => {
+     const index = getRandomIndex(arr);
+     const item = arr.splice(index, 1);
+     return item[0]
+  };
+
+  const getRandomIndex = arr => {
+    return Math.floor(Math.random() * arr.length)
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie of the week</h1>
+        <button onClick={handleSelectedMovieClick}>Choose me a movie</button>
+         {
+          selectedMovie && (
+          <div>
+          <h1>{selectedMovie.name}</h1>
+          <p>Year:{selectedMovie.year}</p>
+          <p>Director:{selectedMovie.director}</p>
+          <p>Description:{selectedMovie.description}</p>
+          </div>
+         )}
       </header>
       <body>
       <h2><b>To see</b></h2>
@@ -36,7 +62,7 @@ const App = () => {
           <div key={movie.id}>
               <p>{movie.name}</p>
               <p>{movie.year}</p>
-        </div>
+          </div>
         ))
         }
       </p>
