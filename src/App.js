7b7e70a16a488ba31360bc54165a714d67ec9dfc
Erika Contreras <ericontrerasleal2@gmail.com>
2023-06-22 12:06:39 -0500

    Add a new movie: Add the states movieName, movieYear, movieDirector and MovieDescription and the functions handleInputChange and handleAddButtonClick to the App component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,35 @@ const App = () => {
   const [movies, setMovies] = useState(movieList);
   const [seenMovies, setSeenMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState('');
+  const [movieName, setMovieName] = useState('');
+  const [movieYear, setMovieYear] = useState('');
+  const [movieDirector, setMovieDirector] = useState('');
+  const [movieDescription, setMovieDescription] = useState('');
+
+  
+  const handleAddButtonClick = () => {
+    setMovies(mv => [...mv, {id: getRandomInt(1000), name: movieName, year: movieYear, director: movieDirector, description: movieDescription}]);
+    setMovieName('');
+    setMovieYear('');
+    setMovieDirector('');
+    setMovieDescription('');
+  };
+
+  const handleInputChange = e => {
+    if (e.target.name === 'movieName'){
+      setMovieName(e.target.value);
+    } else if (e.target.name === 'movieYear'){
+      setMovieYear(e.target.value);
+    } else if (e.target.name === 'movieDirector'){
+      setMovieDirector(e.target.value);
+    } else if (e.target.name === 'movieDescription'){
+      setMovieDescription(e.target.value);
+    }; 
+  };
+
+  const getRandomInt = max => {
+    return Math.floor(Math.random() * max)
+  };
 
   const deleteMovie = movieId => {
     setSeenMovies(mv => [...mv, movies.find(mv => mv.id === movieId)]);
@@ -36,11 +65,21 @@ const App = () => {
           selectedMovie && (
           <div>
           <h1>{selectedMovie.name}</h1>
-          <p>Year:{selectedMovie.year}</p>
-          <p>Director:{selectedMovie.director}</p>
-          <p>Description:{selectedMovie.description}</p>
+          <p>Year: {selectedMovie.year}</p>
+          <p>Director: {selectedMovie.director}</p>
+          <p>Description: {selectedMovie.description}</p>
           </div>
          )}
+         
+         <p>Name:</p>
+         <input name='movieName' onChange={handleInputChange} value={movieName}/>
+         <p>Year:</p>
+         <input name='movieYear' onChange={handleInputChange} value={movieYear}/>
+         <p>Director:</p>
+         <input name='movieDirector' onChange={handleInputChange} value={movieDirector}/>
+         <p>Description:</p>
+         <input name='movieDescription' onChange={handleInputChange} value={movieDescription}/>
+         <button onClick={handleAddButtonClick}>Add Movie</button>
       </header>
       <body>
       <h2><b>To see</b></h2>
